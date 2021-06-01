27e56a99d5c4bed98693b9176d9dd3f638dd4442
Allana Evellyn <allanaevellynm@gmail.com>
2021-06-01 13:51:11 -0300

    Botões

---
+++ entrega-projeto/allana-evellyn/assets/js/app.js
@@ -0,0 +1,10 @@
+//evento de click do botão de pesquisa//
+document.getElementById("navbar__search").addEventListener("click", function(){
+  alert("funcionou!");
+});
+
+
+
+document.getElementById("navbar__input").addEventListener("keydown", function(){
+  console.log("funcionou")
+})
