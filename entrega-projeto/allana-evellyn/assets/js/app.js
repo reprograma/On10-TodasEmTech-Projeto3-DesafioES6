a21fa4325945293dfbbaefcdeea577e3c6c6d1e7
Allana Evellyn <allanaevellynm@gmail.com>
2021-06-16 18:33:31 -0300

    criação do card inicial e requisição da api

--- entrega-projeto/allana-evellyn/assets/js/app.js
+++ entrega-projeto/allana-evellyn/assets/js/app.js
@@ -1,10 +1,52 @@
-//evento de click do botão de pesquisa//
-document.getElementById("navbar__search").addEventListener("click", function(){
-  alert("funcionou!");
+//LINK API
+//const baseURL = 'https://api.github.com/';
+const form = document.querySelector('form');
+const input = document.getElementById('search');
+
+//user's card elements
+const imgUser = document.getElementById('image')
+const userName = document.getElementById('name')
+const userUsername = document.getElementById('username')
+const userDescription = document.getElementById('description')
+const userFollowers = document.getElementById('followers')
+const userRepositories = document.getElementById('repositories')
+
+
+
+//search event
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const usuario = input.value.trim();
+
+  if(usuario){
+    return getUsuario(usuario)
+  }
+    return alert('informe o usuário')
 });
 
+const getUsuario = async (usuario) => {
+  
+  const requisicao = await fetch(`https://api.github.com/users/${usuario}`)
+  
+  const infosUser = await requisicao.json()
+    const dados = infosUser[0];
+
+    console.log(dados.login)
 
 
-document.getElementById("navbar__input").addEventListener("keydown", function(){
-  console.log("funcionou")
-})
+  if(requisicao.status === 404) {
+    alert('Usuário não encontrado!')
+  }
+}
+
+
+
+//user card
+const createCard = (image, name, username, description, followers, repositories) => {
+  imgUser.setAttribute('src', image);
+  userName.innerText = name;
+  userUsername.innerText = username;
+  userDescription.innerText = description;
+  userFollowers.innerText = followers;
+  userRepositories.innerText = repositories;
+}
