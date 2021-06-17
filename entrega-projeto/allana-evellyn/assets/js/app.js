764ceb9863cf96bc794b37cb8593fa6a460fac8b
Allana Evellyn <allanaevellynm@gmail.com>
2021-06-16 21:17:54 -0300

    Criação do card de usuário com as informações cedidas pela API

--- entrega-projeto/allana-evellyn/assets/js/app.js
+++ entrega-projeto/allana-evellyn/assets/js/app.js
@@ -3,7 +3,7 @@
 const form = document.querySelector('form');
 const input = document.getElementById('search');
 
-//user's card elements
+//elementos para o card de usuário
 const imgUser = document.getElementById('image')
 const userName = document.getElementById('name')
 const userUsername = document.getElementById('username')
@@ -13,7 +13,7 @@ const userRepositories = document.getElementById('repositories')
 
 
 
-//search event
+//evento de busca
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const usuario = input.value.trim();
@@ -24,24 +24,28 @@ form.addEventListener('submit', (event) => {
     return alert('informe o usuário')
 });
 
+
+//consumindo a API
 const getUsuario = async (usuario) => {
   
   const requisicao = await fetch(`https://api.github.com/users/${usuario}`)
-  
-  const infosUser = await requisicao.json()
-    const dados = infosUser[0];
+    
+    if(requisicao.status === 404) {
+      alert('Usuário não encontrado!')
+    } else{
 
-    console.log(dados.login)
+      const infosUser = await requisicao.json()
+      const dados = infosUser[0];
 
-
-  if(requisicao.status === 404) {
-    alert('Usuário não encontrado!')
-  }
+      //criando card com as informações cedidas pela api
+      createCard(infosUser.avatar_url, infosUser.name, infosUser.login, infosUser.bio, infosUser.followers, infosUser.public_repos)
+      
+    }
 }
 
 
 
-//user card
+//card de usuário
 const createCard = (image, name, username, description, followers, repositories) => {
   imgUser.setAttribute('src', image);
   userName.innerText = name;
